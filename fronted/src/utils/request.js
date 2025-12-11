406792382d53c94eff3fa8316d133bfe1609147a
Boyang <ba1g22@soton.ac.uk>
2025-12-11 15:43:25 +0800

    服务器功能完善

--- fronted/src/utils/request.js
+++ fronted/src/utils/request.js
@@ -14,8 +14,24 @@ function checkStatus(response) {
   throw error;
 }
 
-export default function request(url, options) {
-  return fetch(url, options)
+export default function request(url, options = {}) {
+  // 设置默认headers
+  const defaultOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  // 合并选项
+  const newOptions = { ...defaultOptions, ...options };
+  
+  // 如果有data且是POST/PUT请求，自动将data转为JSON字符串
+  if (newOptions.data && (newOptions.method === 'POST' || newOptions.method === 'PUT')) {
+    newOptions.body = JSON.stringify(newOptions.data);
+    delete newOptions.data;
+  }
+
+  return fetch(url, newOptions)
     .then(checkStatus)
     .then(parseJSON)
     .then(data => data)
